38f03c052f4be807de83ca1f57306b07dea22f4c
yassimortensen <yassimortensen@gmail.com>
2017-11-13 17:21:01 -0500

    merged with draft

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20171113203020) do
-=======
 ActiveRecord::Schema.define(version: 20171113211933) do
->>>>>>> c3a7a88eb9ab0e10cd99d88fde3e245f7d72060f
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -53,7 +49,6 @@ ActiveRecord::Schema.define(version: 20171113211933) do
     t.string "instagram_account_link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
   end
 
 end
