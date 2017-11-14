6369b757f98f0c8d06c77c1ac23147768edfebf8
yassimortensen <yassimortensen@gmail.com>
2017-11-14 10:54:53 -0500

    added notes to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 
-pet_usernames = [
+pet_usernames = [ #array of all instagram handles
   "dogs.lovers",
   "loki_the_wolfdog",
   "coconutricebear",
@@ -13,17 +13,16 @@ pet_usernames = [
   "ranmathecat",
   "hiimskunk",
   "moggsy_the_cat",
-  "sis.twins",
-  "littlelordreginald",
+  "sis.twins"
 ]
 
-pet_usernames.each do |username|
-  pet = Pet.find_or_create_by(instagram_handle: username)
-  doc = Nokogiri::HTML(open("https://websta.me/n/#{username}"))
+pet_usernames.each do |username| #iterate through the instagram handles
+  pet = Pet.find_or_create_by(instagram_handle: username) #create Pet objects for each
+  doc = Nokogiri::HTML(open("https://websta.me/n/#{username}")) #get HTML
    i = 0
-  doc.css(".list-img img").each do |node|
-    break if i == 21
-    Photo.create(pet: pet, url: node.attribute("src").value)
+  doc.css(".list-img img").each do |node| #iterate through each image on feed
+    break if i == 21 #stop after 20 images
+    Photo.create(pet: pet, url: node.attribute("src").value) #create photo objects for each photo, set up relationship
     i += 1
   end
 end
